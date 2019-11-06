7406d0a96a1f9f1eb78afb9287170ffc0c98607d
Isaiah Odhner <isaiahodhner@gmail.com>
2019-11-06 15:59:21 -0500

    Keep image in place

--- src/injected.js
+++ src/injected.js
@@ -10,15 +10,14 @@
 	window.superrez_image = superrez_image;
 	window.superrez_image_in_place = superrez_image_in_place;
 	
-	function superrez_image_in_place(input_img) {
-		// TODO: keep styling/attributes of img
-		// probably just replace img src
-		superrez_image(input_img, (err, superrezzed_img)=> {
+	function superrez_image_in_place(page_img) {
+		superrez_image(page_img, (err, superrezzed_img)=> {
 			if (err) {
 				return show_error_message("Failed to superrez image", err);
 			}
-			input_img.parentElement.insertBefore(superrezzed_img, input_img);
-			input_img.remove();
+			page_img.style.width = getComputedStyle(page_img).width;
+			page_img.style.height = getComputedStyle(page_img).height;
+			page_img.src = superrezzed_img.src;
 		});
 	}
 	
