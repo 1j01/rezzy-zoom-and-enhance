d83623e80dc892bf4fc73b2156584231fd102054
Isaiah Odhner <isaiahodhner@gmail.com>
2019-11-06 01:37:54 -0500

    WIP

--- src/electron-injected.js
+++ src/electron-injected.js
@@ -1,16 +1,17 @@
 // Electron-specific code injected into the renderer process
 // to provide integrations, for the desktop app
 
+(()=> {
+
 // so libraries don't get confused and export to `module` instead of the `window`
 global.module = undefined;
 
 const is_dev = require("electron-is-dev");
-const dialog = require("electron").remote.dialog;
 const fs = require("fs");
 const path = require("path");
 const argv = require("electron").remote.process.argv;
 
-window.open_from_file_path = (file_path, callback, canceled) => {
+window.read_file = (file_path, callback) => {
 	fs.readFile(file_path, (err, buffer) => {
 		if(err){
 			return callback(err);
@@ -21,77 +22,58 @@ window.open_from_file_path = (file_path, callback, canceled) => {
 			value: file_path,
 		});
 
-		open_from_File(file, callback, canceled);
+		callback(null, file);
 	});
 };
 
-window.save_to_file_path = (filePath, formatName, savedCallback) => {
-	const mimeType = {
-		"JPEG": "image/jpeg",
-		"PNG": "image/png",
-		"GIF": "image/gif",
-		"WebP": "image/webp",
-		// "Monochrome Bitmap": "image/bitmap",
-		// "16 Color Bitmap": "image/bitmap",
-		// "256 Color Bitmap": "image/bitmap",
-		// "24-bit Bitmap": "image/bitmap",
-	}[formatName];
-	if(!mimeType){
-		return show_error_message(`Can't save as ${formatName}. Format is not supported.`);
-	}
-	// if(mimeType === "image/gif"){
-	// 	new GIF();
-	// }
+window.write_canvas_to_file = (file_path, callback) => {
 	canvas.toBlob(blob => {
 		if(blob.type !== mimeType){
-			return show_error_message(`Failed to save as ${formatName} (your browser doesn't support exporting a canvas as "${mimeType}")`);
+			return show_error_message(`Failed to save as ${mimeType} (got "${blob.type}" instead)`);
 		}
-		sanity_check_blob(blob, () => {
-			blob_to_buffer(blob, (err, buffer) => {
+		blob_to_buffer(blob, (err, buffer) => {
+			if(err){
+				return show_error_message("Failed to save! (Technically, failed to convert a Blob to a Buffer.)", err);
+			}
+			fs.writeFile(file_path, buffer, err => {
 				if(err){
-					return show_error_message("Failed to save! (Technically, failed to convert a Blob to a Buffer.)", err);
+					return show_error_message("Failed to save file!", err);
 				}
-				fs.writeFile(filePath, buffer, err => {
-					if(err){
-						return show_error_message("Failed to save file!", err);
-					}
-					const fileName = path.basename(filePath);
-					savedCallback(filePath, fileName);
-				});
+				callback();
 			});
 		});
 	}, mimeType);
 };
 
 
-	const dataPath = require('electron').remote.app.getPath("userData");
+const tempDir = require('electron').remote.app.getPath("temp");
+// const cacheDir = require('electron').remote.app.getPath("cache"); // TODO: is this a good dir to use?
+const cacheDir = require("path").join(require('electron').remote.app.getPath("appData"), "superrez-cache");
 
-	const imgPath = require("path").join(dataPath, "bg.png");
-	const fs = require("fs");
-	const wallpaper = require("wallpaper");
+const imgId = Math.random(); // TODO!
+const inputImgPath = require("path").join(tempDir, `${imgId}-normal-rez.png`);
+const outputImgPath = require("path").join(cacheDir, `${imgId}-superrez.png`);
 
-	// TODO: implement centered option for Windows and Linux in https://www.npmjs.com/package/wallpaper
-	// currently it's only supported on macOS
-	let wallpaperCanvas;
-	if(process.platform === "darwin"){
-		wallpaperCanvas = c;
-	}else{
-		wallpaperCanvas = make_canvas(screen.width, screen.height);
-		const x = (screen.width - c.width) / 2;
-		const y = (screen.height - c.height) / 2;
-		wallpaperCanvas.ctx.drawImage(c, ~~x, ~~y);
-	}
 
-	get_array_buffer_from_canvas(wallpaperCanvas).then(array_buffer => {
-		const buffer = new Buffer(array_buffer);
-		fs.writeFile(imgPath, buffer, err => {
-			if(err){
-				return show_error_message("Failed to set as desktop background: couldn't write temporary image file.", err);
-			}
-		});
-	});
-};
+function blob_to_buffer(blob, callback) {
+	const file_reader = new FileReader()
+
+	file_reader.addEventListener("loadend", event => {
+		if (file_reader.error) {
+			callback(file_reader.error)
+		} else {
+			callback(null, new Buffer(file_reader.result))
+		}
+	}, false)
+
+	// Read the blob as a typed array.
+	file_reader.readAsArrayBuffer(blob)
+
+	return file_reader
+}
 
 function show_error_message(message, error) {
 	alert(`${message}\n\n${error}`);
 }
+
+})();
