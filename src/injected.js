b72a52c633e36e5d0f9e8208793c6495de509f69
Isaiah Odhner <isaiahodhner@gmail.com>
2019-11-06 23:04:53 -0500

    WIP

--- src/injected.js
+++ src/injected.js
@@ -10,28 +10,25 @@
 	let dynamic_queue = []; // dynamic as in not static; it will get sorted before items are pulled
 
 	function superrez_image_in_place(page_img) {
-		superrez_image(page_img, (err, superrezzed_img)=> {
-			if (err) {
-				return show_error_message("Failed to superrez image", err);
-			}
-			page_img.style.width = getComputedStyle(page_img).width;
-			page_img.style.height = getComputedStyle(page_img).height;
-			page_img.src = superrezzed_img.src;
-		});
+		return new Promise((resolve, reject)=> {
+			superrez_image(page_img, (err, superrezzed_img)=> {
+				if (err) {
+					reject(err);
+				}
+				page_img.style.width = getComputedStyle(page_img).width;
+				page_img.style.height = getComputedStyle(page_img).height;
+				page_img.src = superrezzed_img.src;
+				resolve();
+			});
+		})
 	}
 	
 	function show_error_message(message, error) {
 		alert(`${message}\n\n${error}`);
 	}
 
-	function enhance_page() {
-		console.log("enhance page");
-		// TODO: look for background-images as well
-
-		const imgs = Array.from(document.querySelectorAll("img"));
-
-		dynamic_queue = dynamic_queue.concat(imgs);
-
+	function filter_and_sort_queue() {
+		
 		const area = (img)=> img.width * img.height; // TODO: naturalWidth/naturalHeight?
 		// const inDOM = (element)=> element.parentElement != null;
 		const isVisible = (element)=> {
@@ -68,10 +65,27 @@
 			if (area(b) > area(a)) return +1;
 			return 0;
 		});
+	}
 
-		const next_img = dynamic_queue[0];
+	async function enhance_page() {
+		console.log("enhance page");
 
-		superrez_image_in_place(next_img);
+		// TODO: apply to background-images as well
+
+		const imgs = Array.from(document.querySelectorAll("img"));
+
+		dynamic_queue = dynamic_queue.concat(imgs);
+
+		while (dynamic_queue.length > 0) {
+			filter_and_sort_queue();
+			const next_img = dynamic_queue.shift();
+			console.log("next_img", next_img);
+			try {
+				await superrez_image_in_place(next_img);
+			} catch(error) {
+				show_error_message("Failed to superrez image", error);
+			}
+		}
 	}
 	
 	window.addEventListener("load", enhance_page);
