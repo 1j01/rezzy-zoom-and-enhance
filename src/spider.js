895e570e25d0c89857fe3515cc5539a4818079cb
Isaiah Odhner <isaiahodhner@gmail.com>
2019-11-09 09:34:49 -0500

    Refactor
    - Get ready for starting spidering from a URL
    - Fix console message copy pasta error

--- src/spider.js
+++ src/spider.js
@@ -1,5 +1,22 @@
-module.exports.spiderFromHTML = (html, {backwardPages, forwardPages, addJob})=> {
-	var dummy_element = document.createElement( 'html' );
+const spiderFromURL = (url, {backwardPages, forwardPages, addJob})=> {
+	const direction = backwardPages > 0 ?
+		(forwardPages > 0 ? "from start" : "backwards") :
+		(forwardPages > 0 ? "forwards" : "on last page to scrape in this direction")
+	require("request")(url, (error, response, body)=> {
+		if (error) {
+			console.error(`[spider] Failed to get ${url} - stopping scraping (${direction})`);
+			return;
+		}
+		if (response.statusCode !== 200) {
+			console.error(`[spider] Failed to get ${url} - recieved HTTP ${response.statusCode} - stopping scraping (${direction})`);
+			return;
+		}
+		module.exports.spiderFromHTML(body, {backwardPages, forwardPages, addJob});
+	});
+};
+
+const spiderFromHTML = (html, {backwardPages, forwardPages, addJob})=> {
+	var dummy_element = document.createElement("html");
 	dummy_element.innerHTML = html;
 
 	const images = Array.from(dummy_element.getElementsByTagName("img"));
@@ -7,21 +24,21 @@ module.exports.spiderFromHTML = (html, {backwardPages, forwardPages, addJob})=> 
 
 	// TODO: look for main image link
 	const nextLinks = links.filter((a)=>
-		a.outerHTML.match(/next(?![da])|forward|fr?wr?d/i)
+		!!a.outerHTML.match(/next(?![da])|forward|fr?wr?d/i)
 	);
 	const prevLinks = links.filter((a)=>
-		a.outerHTML.match(/prev(?!iew|[eau])|backward|back(\b|[_-])|backwd|bckwd/i)
+		!!a.outerHTML.match(/prev(?!iew|[eau])|backward|back(\b|[_-])|backwd|bckwd/i)
 	);
 	const prioritizePageLinksFirst = (a, b)=> {
 		const ch_regexp = /chapter|chap(\b|[_-])|(\b|[_-])ch(\b|[_-])/i;
 		const pg_regexp = /page/i;
 		const comic_regexp = /comic/i;
-		const a_is_ch = a.outerHTML.match(ch_regexp);
-		const b_is_ch = b.outerHTML.match(ch_regexp);
-		const a_is_pg = a.outerHTML.match(pg_regexp);
-		const b_is_pg = b.outerHTML.match(pg_regexp);
-		const a_is_comic = a.outerHTML.match(comic_regexp);
-		const b_is_comic = b.outerHTML.match(comic_regexp);
+		const a_is_ch = !!a.outerHTML.match(ch_regexp);
+		const b_is_ch = !!b.outerHTML.match(ch_regexp);
+		const a_is_pg = !!a.outerHTML.match(pg_regexp);
+		const b_is_pg = !!b.outerHTML.match(pg_regexp);
+		const a_is_comic = !!a.outerHTML.match(comic_regexp);
+		const b_is_comic = !!b.outerHTML.match(comic_regexp);
 
 		// deprioritize, but don't exclude chapter buttons;
 		// a webcomic could have entire chapters on a page
@@ -67,18 +84,7 @@ module.exports.spiderFromHTML = (html, {backwardPages, forwardPages, addJob})=> 
 	if (backwardPages > 0) {
 		const prevLink = prevLinks[0];
 		if (prevLink) {
-			const url = prevLink.href;
-			require("request")(url, (error, response, body)=> {
-				if (error) {
-					console.error(`[spider] Failed to get ${url} - stopping scraping (backwards)`);
-					return;
-				}
-				if (response.statusCode !== 200) {
-					console.error(`[spider] Failed to get ${url} - recieved HTTP ${response.statusCode} - stopping scraping (forwards)`);
-					return;
-				}
-				module.exports.spiderFromHTML(body, {backwardPages: backwardPages - 1, forwardPages: 0, addJob});
-			});
+			spiderFromURL(prevLink.href, {backwardPages: backwardPages - 1, forwardPages: 0, addJob});
 		} else {
 			console.warn("[spider] No previous page link found");
 		}
@@ -88,20 +94,11 @@ module.exports.spiderFromHTML = (html, {backwardPages, forwardPages, addJob})=> 
 	if (forwardPages > 0) {
 		const nextLink = nextLinks[0];
 		if (nextLink) {
-			const url = nextLink.href;
-			require("request")(url, (error, response, body)=> {
-				if (error) {
-					console.error(`[spider] Failed to get ${url} - stopping scraping (forwards)`);
-					return;
-				}
-				if (response.statusCode !== 200) {
-					console.error(`[spider] Failed to get ${url} - recieved HTTP ${response.statusCode} - stopping scraping (forwards)`);
-					return;
-				}
-				module.exports.spiderFromHTML(body, {backwardPages: 0, forwardPages: forwardPages - 1, addJob});
-			});
+			spiderFromURL(nextLink.href, {backwardPages: 0, forwardPages: forwardPages - 1, addJob});
 		} else {
 			console.warn("[spider] No next page link found");
 		}
 	}
 };
+
+module.exports = {spiderFromURL, spiderFromHTML};
