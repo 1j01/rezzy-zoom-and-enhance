af1b4bed3a828e76d6092b37db9da4656b832c0f
Isaiah Odhner <isaiahodhner@gmail.com>
2019-11-18 10:33:03 -0500

    express

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 4284;
+
+const app = express();
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES FOR OUR API
+// =============================================================================
+const router = express.Router();
+
+// test route to make sure everything is working (accessed at GET http://localhost:4284/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
+
